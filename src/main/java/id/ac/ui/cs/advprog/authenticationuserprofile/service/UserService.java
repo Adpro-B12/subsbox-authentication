620c19a43f44cb6f7a10c02abf8531bbe7a76ac4
nandanathaniela <124995047+nandanathaniela@users.noreply.github.com>
2024-04-24 06:19:24 +0700

    [GREEN] Implementation for UserService

--- src/main/java/id/ac/ui/cs/advprog/authenticationuserprofile/service/UserService.java
+++ src/main/java/id/ac/ui/cs/advprog/authenticationuserprofile/service/UserService.java
@@ -1,4 +1,11 @@
 package id.ac.ui.cs.advprog.authenticationuserprofile.service;
 
+import id.ac.ui.cs.advprog.authenticationuserprofile.model.User;
+
+import java.util.UUID;
 public interface UserService {
+    User registerNewUser(User user);
+    User updateUser(User user);
+    void deleteUser(UUID userId);
+    User getUserByEmail(String email);
 }
