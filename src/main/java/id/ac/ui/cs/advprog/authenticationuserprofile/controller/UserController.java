e9f46fbf022555125024f05738f2cfb4ea4eb5c2
nandanathaniela <124995047+nandanathaniela@users.noreply.github.com>
2024-04-24 06:54:01 +0700

    [GREEN] Implementation UserController

--- src/main/java/id/ac/ui/cs/advprog/authenticationuserprofile/controller/UserController.java
+++ src/main/java/id/ac/ui/cs/advprog/authenticationuserprofile/controller/UserController.java
@@ -1,4 +1,40 @@
 package id.ac.ui.cs.advprog.authenticationuserprofile.controller;
 
+import id.ac.ui.cs.advprog.authenticationuserprofile.model.User;
+import id.ac.ui.cs.advprog.authenticationuserprofile.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User newUser = userService.registerNewUser(user);
+        return ResponseEntity.ok(newUser);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        User updatedUser = userService.updateUser(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable UUID userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        User user = userService.getUserByEmail(email);
+        return ResponseEntity.ok(user);
+    }
 }
