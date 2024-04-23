cb579963e5119518247748b197c5ad189f44d4d9
nandanathaniela <124995047+nandanathaniela@users.noreply.github.com>
2024-04-24 06:07:56 +0700

    [RED] Make test for user model

---
+++ src/test/java/id/ac/ui/cs/advprog/authenticationuserprofile/model/UserTest.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.advprog.authenticationuserprofile.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UserTest {
+    User user;
+    @Test
+    void testGetUserID() {
+    }
+
+    @Test
+    void testGetEmail() {
+    }
+
+    @Test
+    void testGetPassword() {
+    }
+
+    @Test
+    void testGetFullName() {
+    }
+
+    @Test
+    void testGetPhoneNumber() {
+    }
+
+    @Test
+    void testGetAddress() {
+    }
+}
