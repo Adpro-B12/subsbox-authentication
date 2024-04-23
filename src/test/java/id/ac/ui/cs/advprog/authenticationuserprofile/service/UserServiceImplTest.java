6674d50f958196631eb1878e20ca076599534d38
nandanathaniela <124995047+nandanathaniela@users.noreply.github.com>
2024-04-24 06:52:40 +0700

    [RED] Make test for UserServiceImpl

---
+++ src/test/java/id/ac/ui/cs/advprog/authenticationuserprofile/service/UserServiceImplTest.java
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.advprog.authenticationuserprofile.service;
+
+public class UserServiceImplTest {
+
+    void setUp() {
+
+    }
+
+
+    void whenRegisterUser_thenSaveUser() {
+
+    }
+
+    void whenUpdateUser_thenUpdateUserDetails() {
+
+    }
+
+    void whenDeleteUser_thenRepositoryDeleteCalled() {
+
+    }
+
+    void whenGetUserByEmail_thenReturnUser() {
+
+    }
+}
