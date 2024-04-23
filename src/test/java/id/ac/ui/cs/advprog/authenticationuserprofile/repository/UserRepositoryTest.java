5ca7b9e5ea829e341a08c204a222026915598191
nandanathaniela <124995047+nandanathaniela@users.noreply.github.com>
2024-04-24 06:50:57 +0700

    [RED] Make test for UserRepository

---
+++ src/test/java/id/ac/ui/cs/advprog/authenticationuserprofile/repository/UserRepositoryTest.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.advprog.authenticationuserprofile.repository;
+
+public class UserRepositoryTest {
+
+    void setUp() {
+
+    }
+
+    void shouldSaveUser() {
+
+    }
+
+    void shouldFindUserByEmail() {
+
+    }
+
+    void shouldNotAllowDuplicateEmail() {
+
+    }
+
+    void shouldUpdateUser() {
+
+    }
+
+    void shouldDeleteUser() {
+
+    }
+}
