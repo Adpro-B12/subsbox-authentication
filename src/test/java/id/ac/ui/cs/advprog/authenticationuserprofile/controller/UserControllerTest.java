575b0aa48d2f3e3e21320b0de7b57a8fd10b52e2
nandanathaniela <124995047+nandanathaniela@users.noreply.github.com>
2024-04-24 06:50:26 +0700

    [RED] Make test for UserController

---
+++ src/test/java/id/ac/ui/cs/advprog/authenticationuserprofile/controller/UserControllerTest.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.authenticationuserprofile.controller;
+
+public class UserControllerTest {
+
+    public void setup() {
+
+    }
+
+    public void testRegisterUser() throws Exception {
+
+    }
+
+    public void testUpdateUser() throws Exception {
+
+    }
+
+    public void testDeleteUser() throws Exception {
+
+    }
+
+    public void testGetUserByEmail() throws Exception {
+
+    }
+}
