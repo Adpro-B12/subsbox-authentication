00120958ee4cca6de88dddf5715117934e4f5a7d
nandanathaniela <124995047+nandanathaniela@users.noreply.github.com>
2024-05-22 20:17:29 +0700

    [REFACTOR] Update application.properties to database local postgres

--- src/main/java/id/ac/ui/cs/advprog/authenticationuserprofile/AuthenticationUserProfileApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/authenticationuserprofile/AuthenticationUserProfileApplication.java
@@ -3,7 +3,7 @@ package id.ac.ui.cs.advprog.authenticationuserprofile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "id.ac.ui.cs.advprog.authenticationuserprofile")
 public class AuthenticationUserProfileApplication {
 
     public static void main(String[] args) {
