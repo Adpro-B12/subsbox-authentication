c334a215779ef1933fca3de11b560662aa587422
alifbintang1 <alifbintange@gmail.com>
2024-05-25 06:17:52 +0700

    add active profiles test

--- src/test/java/id/ac/ui/cs/advprog/authenticationuserprofile/AuthenticationUserProfileApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/authenticationuserprofile/AuthenticationUserProfileApplicationTests.java
@@ -3,6 +3,9 @@ package id.ac.ui.cs.advprog.authenticationuserprofile;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 @SpringBootTest
 class AuthenticationUserProfileApplicationTests {
 
