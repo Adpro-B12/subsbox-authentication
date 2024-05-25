06fb0799236fd44f54b4a7699e0d555e94e4463c
alifbintang1 <alifbintange@gmail.com>
2024-05-25 10:22:25 +0700

    add scan base packages back

--- src/main/java/id/ac/ui/cs/advprog/authenticationuserprofile/AuthenticationUserProfileApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/authenticationuserprofile/AuthenticationUserProfileApplication.java
@@ -3,8 +3,7 @@ package id.ac.ui.cs.advprog.authenticationuserprofile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication(scanBasePackages = "id.ac.ui.cs.advprog.authenticationuserprofile")
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "id.ac.ui.cs.advprog.authenticationuserprofile")
 public class AuthenticationUserProfileApplication {
 
     public static void main(String[] args) {
